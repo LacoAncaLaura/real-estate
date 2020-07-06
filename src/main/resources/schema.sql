a7955572c7a5caab5051816e5e10bbb06ccceb96
LacoAncaLaura <lala14444444@yahoo.com>
2020-07-06 15:11:13 +0300

    .

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,14 +1,15 @@
-DROP TABLE IF EXISTS TBL_ESTATE;
-
-CREATE TABLE TBL_ESTATE(
+DROP TABLE IF EXISTS estate;
+create table estate
+(
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    type VARCHAR(255) NOT NULL,
-    city VARCHAR(255) NOT NULL,
-    street VARCHAR(255) NOT NULL,
+    type VARCHAR(250) NOT NULL,
+    city VARCHAR(250) NOT NULL,
+    street VARCHAR(250) NOT NULL,
     number INT(11) NOT NULL,
-    owner VARCHAR(255) NOT NULL,
+    owner VARCHAR(250) NOT NULL,
     size DOUBLE NOT NULL,
     value INT(11) NOT NULL,
     quantity INT(11) NOT NULL,
-    tax DOUBLE NOT NULL,
-)
+    tax DOUBLE NOT NULL);
+INSERT INTO estate (type,city,street,number,owner,size,value,quantity,tax)VALUES
+('House', 'Dublin','MainStreet',5,'John',80.5,100000,1,156);
